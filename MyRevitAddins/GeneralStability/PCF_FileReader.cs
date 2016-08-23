7e61da197a931145f9dabbcd340e95d010edd02b
Michail Golubjev <xopkop@gmail.com>
2016-08-23 22:13:15 +0200

    Initial setup 1

---
+++ MyRevitAddins/GeneralStability/PCF_FileReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Revit_PCF_Importer
+{
+    class FileReader
+    {
+        string[] readLines;
+
+        public string[] ReadFile(string path)
+        {
+
+            readLines = System.IO.File.ReadAllLines(path);
+            return readLines;
+        }
+    }
+}
