1ae4c4bfc5ade03e39dafc88e78c1ec03e196d20
Michail Golubjev <xopkop@gmail.com>
2016-09-20 23:03:01 +0200

    Optimization finished - changed data structure to HashSet and halved the execution time. Cannot find any other things I can optimize. Back to debugging the algorithm.

--- MyRevitAddins/GeneralStabilityPrime/InteractionRevit.cs
+++ MyRevitAddins/GeneralStabilityPrime/InteractionRevit.cs
@@ -128,10 +128,10 @@ namespace GeneralStability
                     for (int j = 0; j < nrOfY; j++)
                     {
                         //Optimize
-                        sbLog.Append(nrTotal);
+                        //sbLog.Append(nrTotal);
 
-                        #region Stopwatch1
-                        var watch1 = Stopwatch.StartNew();
+                        //#region Stopwatch1
+                        //var watch1 = Stopwatch.StartNew();
 
                         #region watch1
                         //Current y value
@@ -140,13 +140,13 @@ namespace GeneralStability
                         double yC = y1 + step / 2;
                         #endregion watch1
 
-                        watch1.Stop();
-                        TimeSpan time1 = watch1.Elapsed;
-                        sbLog.Append(", " + time1.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
-                        #endregion
+                        //watch1.Stop();
+                        //TimeSpan time1 = watch1.Elapsed;
+                        //sbLog.Append(", " + time1.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+                        //#endregion
 
-                        #region Stopwatch2
-                        var watch2 = Stopwatch.StartNew();
+                        //#region Stopwatch2
+                        //var watch2 = Stopwatch.StartNew();
 
                         #region watch2
                         //Determine nearest wall
@@ -155,26 +155,26 @@ namespace GeneralStability
 
                         #endregion watch2
 
-                        watch2.Stop();
-                        TimeSpan time2 = watch2.Elapsed;
-                        sbLog.Append(", " + time2.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
-                        #endregion
+                        //watch2.Stop();
+                        //TimeSpan time2 = watch2.Elapsed;
+                        //sbLog.Append(", " + time2.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+                        //#endregion
 
-                        #region Stopwatch3
-                        var watch3 = Stopwatch.StartNew();
+                        //#region Stopwatch3
+                        //var watch3 = Stopwatch.StartNew();
 
                         #region watch3
                         //Area of finite element
                         double areaSqrM = ((x2 - x1) * (y2 - y1)).SqrFeetToSqrMeters();
                         #endregion watch3
 
-                        watch3.Stop();
-                        TimeSpan time3 = watch3.Elapsed;
-                        sbLog.Append(", " + time3.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
-                        #endregion
+                        //watch3.Stop();
+                        //TimeSpan time3 = watch3.Elapsed;
+                        //sbLog.Append(", " + time3.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+                        //#endregion
 
-                        #region Stopwatch4
-                        var watch4 = Stopwatch.StartNew();
+                        //#region Stopwatch4
+                        //var watch4 = Stopwatch.StartNew();
 
                         #region watch4
                         //Determine the correct load intensity at the finite element centre point
@@ -184,13 +184,13 @@ namespace GeneralStability
                         double loadIntensity = 0.0;
                         #endregion watch4
 
-                        watch4.Stop();
-                        TimeSpan time4 = watch4.Elapsed;
-                        sbLog.Append(", " + time4.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
-                        #endregion
+                        //watch4.Stop();
+                        //TimeSpan time4 = watch4.Elapsed;
+                        //sbLog.Append(", " + time4.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+                        //#endregion
 
-                        #region Stopwatch5
-                        var watch5 = Stopwatch.StartNew();
+                        //#region Stopwatch5
+                        //var watch5 = Stopwatch.StartNew();
 
                         #region watch5
                         for (int f = 0; f < faces.Count; f++)
@@ -202,49 +202,49 @@ namespace GeneralStability
                         }
                         #endregion watch5
 
-                        watch5.Stop();
-                        TimeSpan time5 = watch5.Elapsed;
-                        sbLog.Append(", " + time5.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
-                        #endregion
+                        //watch5.Stop();
+                        //TimeSpan time5 = watch5.Elapsed;
+                        //sbLog.Append(", " + time5.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+                        //#endregion
 
-                        #region Stopwatch6
-                        var watch6 = Stopwatch.StartNew();
+                        //#region Stopwatch6
+                        //var watch6 = Stopwatch.StartNew();
 
                         #region watch6
                         double force = loadIntensity * areaSqrM;
                         #endregion watch6
 
-                        watch6.Stop();
-                        TimeSpan time6 = watch6.Elapsed;
-                        sbLog.Append(", " + time6.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
-                        #endregion
+                        //watch6.Stop();
+                        //TimeSpan time6 = watch6.Elapsed;
+                        //sbLog.Append(", " + time6.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+                        //#endregion
 
-                        #region Stopwatch7
-                        var watch7 = Stopwatch.StartNew();
+                        //#region Stopwatch7
+                        //var watch7 = Stopwatch.StartNew();
 
                         #region watch7
                         double currentValue = nearestWall.LookupParameter("GS_Load").AsDouble();
                         #endregion watch7
 
-                        watch7.Stop();
-                        TimeSpan time7 = watch7.Elapsed;
-                        sbLog.Append(", " + time7.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
-                        #endregion
+                        //watch7.Stop();
+                        //TimeSpan time7 = watch7.Elapsed;
+                        //sbLog.Append(", " + time7.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+                        //#endregion
 
-                        #region Stopwatch8
-                        var watch8 = Stopwatch.StartNew();
+                        //#region Stopwatch8
+                        //var watch8 = Stopwatch.StartNew();
 
                         #region watch8
 
                         bool success = nearestWall.LookupParameter("GS_Load").Set(currentValue + force);
                         #endregion watch8
 
-                        watch8.Stop();
-                        TimeSpan time8 = watch8.Elapsed;
-                        sbLog.Append(", " + time8.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
-                        #endregion
+                        //watch8.Stop();
+                        //TimeSpan time8 = watch8.Elapsed;
+                        //sbLog.Append(", " + time8.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+                        //#endregion
 
-                        sbLog.AppendLine();
+                        //sbLog.AppendLine();
 
                         nrTotal++;
 
