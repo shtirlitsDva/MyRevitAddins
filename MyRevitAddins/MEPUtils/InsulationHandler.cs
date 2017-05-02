48039b41a98159722482f73abe33c2898dca107e
Michail Golubjev <xopkop@gmail.com>
2017-05-02 22:48:04 +0200

    Starting up

--- MyRevitAddins/MEPUtils/InsulationHandler.cs
+++ MyRevitAddins/MEPUtils/InsulationHandler.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MoreLinq;
+using System.Data;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Structure;
+using Shared;
+using fi = Shared.Filter;
+using ut = Shared.Util;
+using op = Shared.Output;
+using tr = Shared.Transformation;
+using mp = Shared.MyMepUtils;
 
 namespace MEPUtils
 {
     public class InsulationHandler
     {
+        public Result CreateInsulationForPipes(ExternalCommandData cData)
+        {
+            Document doc = cData.Application.ActiveUIDocument.Document;
+            var allPipes = fi.GetElements<Pipe>(doc);
+
+            var allInsulationTypes = fi.GetElements<PipeInsulationType>(doc);
+            var insulationType = (from PipeInsulationType pit in allInsulationTypes
+                                  where pit.)
+
+            return Result.Succeeded;
+        }
     }
 }
