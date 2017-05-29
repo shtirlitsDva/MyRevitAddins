d4a2fe573f91bb5b62222c886735dee3b0b6ce6d
shtirlitsDva <app@mgtek.dk>
2017-05-29 08:23:22 +0200

    Forgotten commit

--- MyRevitAddins/MEPUtils/InsulationHandler.cs
+++ MyRevitAddins/MEPUtils/InsulationHandler.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using MoreLinq;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Structure;
+using Shared;
+using fi = Shared.Filter;
+using ut = Shared.Util;
+using op = Shared.Output;
+using tr = Shared.Transformation;
+using mp = Shared.MyMepUtils;
 
 namespace MEPUtils
 {
