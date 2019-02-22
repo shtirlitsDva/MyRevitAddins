44de480a6a7444dcec0f233b5a44c693b439be0f
Michail Golubjev <mgo@damgaard-ri.dk>
2019-02-22 20:42:05 +0100

    Remove leftover methods

--- MyRevitAddins/MEPUtils/09 CreateInstrumentation/01 CreateInstrumentation.cs
+++ MyRevitAddins/MEPUtils/09 CreateInstrumentation/01 CreateInstrumentation.cs
@@ -117,7 +117,7 @@ namespace MEPUtils.CreateInstrumentation
                         //Change size of the pipe
                         Parameter par = dummyPipe.get_Parameter(BuiltInParameter.RBS_PIPE_DIAMETER_PARAM);
                         par.Set(size.MmToFt());
-                        
+
                         //Find the connector from the dummy pipe at intersection
                         var cons = mp.GetALLConnectorsFromElements(dummyPipe);
                         Connector con = cons.Where(c => c.Origin.Equalz(iP, Extensions._1mmTol)).FirstOrDefault();
@@ -176,136 +176,6 @@ namespace MEPUtils.CreateInstrumentation
 
 
             return ((Pipe)selectedPipe, point_in_3d);
-        }
-
-        private static (Pipe pipe, Element element) PlaceSupports(ExternalCommandData commandData, string name)
-        {
-            var app = commandData.Application;
-            var uiDoc = app.ActiveUIDocument;
-            var doc = uiDoc.Document;
-
-            try
-            {
-                //Select a pipe
-                var selectedPipe = Shared.BuildingCoder.BuildingCoderUtilities.SelectSingleElementOfType(uiDoc, typeof(Pipe),
-                    "Select a pipe where to place a support!", false);
-                //Get end connectors
-                var conQuery = (from Connector c in mp.GetALLConnectorsFromElements(selectedPipe)
-                                where (int)c.ConnectorType == 1
-                                select c).ToList();
-
-                Connector c1 = conQuery.First();
-                Connector c2 = conQuery.Last();
-
-                //Define a plane by three points
-                //Detect if the pipe concides with X-axis
-                //If true use another axis to define point
-                Plane plane;
-
-                if (c1.Origin.Y.Equalz(c2.Origin.Y, Extensions._epx) && c1.Origin.Z.Equalz(c2.Origin.Z, Extensions._epx))
-                {
-                    plane = Plane.CreateByThreePoints(c1.Origin, c2.Origin, new XYZ(c1.Origin.X, c1.Origin.Y + 5, c1.Origin.Z));
-                }
-                else
-                {
-                    plane = Plane.CreateByThreePoints(c1.Origin, c2.Origin, new XYZ(c1.Origin.X + 5, c1.Origin.Y, c1.Origin.Z));
-                }
-
-                //Set view sketch plane to the be the created plane
-                var sp = SketchPlane.Create(doc, plane);
-                uiDoc.ActiveView.SketchPlane = sp;
-                //Get a 3d point by picking a point
-                XYZ point_in_3d = null;
-                try
-                {
-                    point_in_3d = uiDoc.Selection.PickPoint(
-                      "Please pick a point on the plane"
-                      + " defined by the selected face");
-                }
-                catch (OperationCanceledException)
-                {
-                }
-
-                //Get family symbol
-                //FilteredElementCollector collector = new FilteredElementCollector(doc);
-                //ElementParameterFilter filter = fi.ParameterValueFilter(name, BuiltInParameter.SYMBOL_FAMILY_AND_TYPE_NAMES_PARAM);
-                //LogicalOrFilter classFilter = fi.FamSymbolsAndPipeTypes();
-                //FamilySymbol familySymbol = (FamilySymbol)collector.WherePasses(classFilter).WherePasses(filter).FirstOrDefault();
-
-                FamilySymbol famSym = 
-                    fi.GetElements<FamilySymbol, BuiltInParameter>(doc, BuiltInParameter.SYMBOL_FAMILY_AND_TYPE_NAMES_PARAM, name).FirstOrDefault();
-                if (famSym == null) throw new Exception("No SUPPORT FamilySymbol loaded in project!");
-
-                //The strange symbol activation thingie...
-                //See: http://thebuildingcoder.typepad.com/blog/2014/08/activate-your-family-symbol-before-using-it.html
-                if (!famSym.IsActive)
-                {
-                    famSym.Activate();
-                    doc.Regenerate();
-                }
-
-                //Get the host pipe level
-                Level level = (Level)doc.GetElement(selectedPipe.LevelId);
-
-                //Create the support instance
-                Element support = doc.Create.NewFamilyInstance(point_in_3d, famSym, level, StructuralType.NonStructural);
-
-                //Get the connector from the support
-                ConnectorSet connectorSetToAdd = mp.GetConnectorSet(support);
-                if (connectorSetToAdd.IsEmpty)
-                    throw new Exception("The support family lacks a connector. Please read the documentation for correct procedure of setting up a support element.");
-                Connector connectorToConnect = (from Connector c in connectorSetToAdd select c).FirstOrDefault();
-
-                //Rotate into place
-                tr.RotateElementInPosition(point_in_3d, connectorToConnect, c1, support);
-
-                //Set diameter
-                Parameter nominalDiameter = support.LookupParameter("Nominal Diameter");
-                nominalDiameter.Set(conQuery.First().Radius * 2);
-
-                return ((Pipe)selectedPipe, support);
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-        }
-
-        /// <summary>
-        /// Set the correct PipingSystemType for the placed support family.
-        /// </summary>
-        /// <param name="commandData">The usual ExternalCommandData.</param>
-        /// <param name="pipe">The pipe on which the support was placed.</param>
-        /// <param name="support">The support that was placed.</param>
-        private static (Pipe dummyPipe, Connector supportConnector) SetSystemType(ExternalCommandData commandData, Pipe pipe, Element support)
-        {
-            var app = commandData.Application;
-            var uiDoc = app.ActiveUIDocument;
-            var doc = uiDoc.Document;
-
-            //Get the pipe type from pipe
-            ElementId pipeTypeId = pipe.PipeType.Id;
-
-            //Get system type from pipe
-            ConnectorSet pipeConnectors = pipe.ConnectorManager.Connectors;
-            Connector pipeConnector = (from Connector c in pipeConnectors where (int)c.ConnectorType == 1 select c).FirstOrDefault();
-            ElementId pipeSystemType = pipeConnector.MEPSystem.GetTypeId();
-
-            //Get the connector from the support
-            Connector connectorToConnect = (from Connector c in ((FamilyInstance)support).MEPModel.ConnectorManager.Connectors select c).FirstOrDefault();
-
-            //Create a point in space to connect the pipe
-            XYZ direction = connectorToConnect.CoordinateSystem.BasisZ.Multiply(2);
-            XYZ origin = connectorToConnect.Origin;
-            XYZ pointInSpace = origin.Add(direction);
-
-            //Create the pipe
-            Pipe dummyPipe = Pipe.Create(doc, pipeTypeId, pipe.ReferenceLevel.Id, connectorToConnect, pointInSpace);
-
-            //Change the pipe system type to match the picked pipe (it is not always matching)
-            dummyPipe.SetSystemType(pipeSystemType);
-
-            return (dummyPipe, connectorToConnect);
         }
     }
 }
