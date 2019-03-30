744979d04514c46c748346c456d910959c5c695d
Michail Golubjev <mgo@damgaard-ri.dk>
2019-03-30 22:26:04 +0100

    Reclone solution and disable uicustomization

--- MyRevitAddins/RibbonPanel/App.cs
+++ MyRevitAddins/RibbonPanel/App.cs
@@ -255,15 +255,15 @@ More than two elements selected + CTRL
         }
     }
 
-    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-    class UICusomizationCaller : IExternalCommand
-    {
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
-        {
-            Result result = RevitUICustomization.RevitUICustomization.test(commandData);
-            return result;
-        }
-    }
+    //[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    //class UICusomizationCaller : IExternalCommand
+    //{
+    //    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    //    {
+    //        Result result = RevitUICustomization.RevitUICustomization.test(commandData);
+    //        return result;
+    //    }
+    //}
 
     [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
     class SetMarkCaller : IExternalCommand
