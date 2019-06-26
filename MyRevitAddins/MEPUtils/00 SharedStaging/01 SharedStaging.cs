7e0e36f5586c511fe38d9222b5df28e171ce6902
Michail Golubjev <xopkop@gmail.com>
2019-06-26 08:13:38 +0200

    MPV: New quick tool to split values of tags...

--- MyRevitAddins/MEPUtils/00 SharedStaging/01 SharedStaging.cs
+++ MyRevitAddins/MEPUtils/00 SharedStaging/01 SharedStaging.cs
@@ -43,5 +43,37 @@ namespace MEPUtils.SharedStaging
             }
             else return PartType.Undefined;
         }
+
+        /// <summary>
+        /// Method is taken from here:
+        /// https://spiderinnet.typepad.com/blog/2011/08/revit-parameter-api-asvaluestring-tostring-tovaluestring-and-tovaluedisplaystring.html
+        /// </summary>
+        /// <param name="p">Revit parameter</param>
+        /// <returns>Stringified contents of the parameter</returns>
+        internal static string ToValueString(this Autodesk.Revit.DB.Parameter p)
+        {
+            string ret = string.Empty;
+
+            switch (p.StorageType)
+            {
+                case StorageType.ElementId:
+                    ret = p.AsElementId().ToString();
+                    break;
+                case StorageType.Integer:
+                    ret = p.AsInteger().ToString();
+                    break;
+                case StorageType.String:
+                    ret = p.AsString();
+                    break;
+                case StorageType.Double:
+                    ret = p.AsValueString();
+                    break;
+                default:
+                    break;
+            }
+
+            return ret;
+        }
+
     }
 }
---
+++ MyRevitAddins/MEPUtils/14 ManageParameterValues/01 ManageParameterValues.cs
@@ -0,0 +1,53 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using MoreLinq;
+using System.Windows.Input;
+using System.Collections.Generic;
+using MEPUtils.SharedStaging;
+using static Shared.Filter;
+using static Shared.Extensions;
+
+namespace MEPUtils.ManageParameterValues
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class SplitParameterValue : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            return SplitParameterValues(commandData);
+        }
+
+        internal Result SplitParameterValues(ExternalCommandData cData)
+        {
+            Document doc = cData.Application.ActiveUIDocument.Document;
+            var pipeAccessories = GetElements<Element, BuiltInCategory>(doc, BuiltInCategory.OST_PipeAccessory);
+
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("Split TAGs!");
+                foreach (Element e in pipeAccessories)
+                {
+                    Parameter TAG2Par = e.LookupParameter("TAG 2");
+                    string TAG2Value = TAG2Par.ToValueString();
+
+                    string valueToFindAndSplit = "DVL";
+
+                    if (TAG2Value.Contains(valueToFindAndSplit))
+                    {
+                        Parameter TAG1Par = e.LookupParameter("TAG 1");
+                        TAG1Par.Set(valueToFindAndSplit);
+
+                        TAG2Value = TAG2Value.Replace(valueToFindAndSplit, "");
+                        TAG2Par.Set(TAG2Value);
+                    }
+                }
+                tx.Commit();
+            }
+            return Result.Succeeded;
+        }
+    }
+}
