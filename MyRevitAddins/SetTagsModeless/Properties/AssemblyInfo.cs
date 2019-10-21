1a24e4adbcb3259ea83a93d7a79da07bdb113d71
Michail Golubjev <xopkop@gmail.com>
2019-10-21 07:50:12 +0200

    ModelessForms: Change default namespace

--- MyRevitAddins/SetTagsModeless/Properties/AssemblyInfo.cs
+++ MyRevitAddins/SetTagsModeless/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SetTagsModeless")]
+[assembly: AssemblyTitle("ModelessForms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SetTagsModeless")]
+[assembly: AssemblyProduct("ModelessForms")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2019.0.0.*")]
-[assembly: AssemblyFileVersion("2019.0.0.0")]
+[assembly: AssemblyVersion("2020.0.0.*")]
+[assembly: AssemblyFileVersion("2020.0.0.0")]
