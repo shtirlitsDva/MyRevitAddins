23fa2e06fc7d7073e17c5fd1e8e1b17e4eda6d21
Michail Golubjev <xopkop@gmail.com>
2020-05-11 19:59:26 +0200

    Tilkoblet: Several bugfixes

--- MyRevitAddins/MEPUtils/16 Tilkoblet/01 Tilkoblet.cs
+++ MyRevitAddins/MEPUtils/16 Tilkoblet/01 Tilkoblet.cs
@@ -21,9 +21,11 @@ using fi = Shared.Filter;
 using lad = MEPUtils.CreateInstrumentation.ListsAndDicts;
 using mp = Shared.MepUtils;
 using tr = Shared.Transformation;
+using Autodesk.Revit.Attributes;
 
 namespace MEPUtils.Tilkoblet
 {
+    [Transaction(TransactionMode.Manual)]
     public class Tilkoblet : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
@@ -47,19 +49,19 @@ namespace MEPUtils.Tilkoblet
 
                         Connector refFirstCon = null; Connector refSecondCon = null;
 
-                        if (firstSideCon.IsConnected)
+                        if (firstSideCon != null && firstSideCon.IsConnected)
                         {
                             var refFirstCons = MepUtils.GetAllConnectorsFromConnectorSet(firstSideCon.AllRefs);
                             refFirstCon = refFirstCons.FirstOrDefault();
                         }
-                        else refFirstCon = DetectUnconnectedConnector(doc, firstSideCon);
+                        else if (firstSideCon != null) refFirstCon = DetectUnconnectedConnector(doc, firstSideCon);
 
-                        if (secondSideCon.IsConnected)
+                        if (secondSideCon != null && secondSideCon.IsConnected)
                         {
                             var refSecondCons = MepUtils.GetAllConnectorsFromConnectorSet(secondSideCon.AllRefs);
                             refSecondCon = refSecondCons.FirstOrDefault();
                         }
-                        else refSecondCon = DetectUnconnectedConnector(doc, secondSideCon);
+                        else if (secondSideCon != null) refSecondCon = DetectUnconnectedConnector(doc, secondSideCon);
 
                         Element firstSideOwner = null; Element secondSideOwner = null;
                         string commentsContent = string.Empty;
@@ -69,33 +71,40 @@ namespace MEPUtils.Tilkoblet
                         if (refFirstCon != null)
                         {
                             firstSideOwner = refFirstCon.Owner;
-                            string tag1 = firstSideOwner.LookupParameter("TAG 1").AsString();
-                            string tag2 = firstSideOwner.LookupParameter("TAG 2").AsString();
-                            string tag3 = firstSideOwner.LookupParameter("TAG 3").AsString();
-                            string tag4 = firstSideOwner.LookupParameter("TAG 4").AsString();
+                            Parameter tag1par = firstSideOwner.LookupParameter("TAG 1");
+                            if (tag1par != null)
+                            {
+                                string tag1 = firstSideOwner.LookupParameter("TAG 1").AsString();
+                                string tag2 = firstSideOwner.LookupParameter("TAG 2").AsString();
+                                string tag3 = firstSideOwner.LookupParameter("TAG 3").AsString();
+                                string tag4 = firstSideOwner.LookupParameter("TAG 4").AsString();
 
-                            firstSideComments = string.Empty;
+                                firstSideComments = string.Empty;
 
-                            if (!string.IsNullOrEmpty(tag1)) firstSideComments += tag1;
-                            if (!string.IsNullOrEmpty(tag2)) firstSideComments += "_" + tag2;
-                            if (!string.IsNullOrEmpty(tag3)) firstSideComments += "_" + tag3;
-                            if (!string.IsNullOrEmpty(tag4)) firstSideComments += "_" + tag4;
+                                if (!string.IsNullOrEmpty(tag1)) firstSideComments += tag1;
+                                if (!string.IsNullOrEmpty(tag2)) firstSideComments += "_" + tag2;
+                                if (!string.IsNullOrEmpty(tag3)) firstSideComments += "_" + tag3;
+                                if (!string.IsNullOrEmpty(tag4)) firstSideComments += "_" + tag4;
+                            }
                         }
-
                         if (refSecondCon != null)
                         {
                             secondSideOwner = refSecondCon.Owner;
-                            string tag1 = secondSideOwner.LookupParameter("TAG 1").AsString();
-                            string tag2 = secondSideOwner.LookupParameter("TAG 2").AsString();
-                            string tag3 = secondSideOwner.LookupParameter("TAG 3").AsString();
-                            string tag4 = secondSideOwner.LookupParameter("TAG 4").AsString();
+                            Parameter tag1par = secondSideOwner.LookupParameter("TAG 1");
+                            if (tag1par != null)
+                            {
+                                string tag1 = secondSideOwner.LookupParameter("TAG 1").AsString();
+                                string tag2 = secondSideOwner.LookupParameter("TAG 2").AsString();
+                                string tag3 = secondSideOwner.LookupParameter("TAG 3").AsString();
+                                string tag4 = secondSideOwner.LookupParameter("TAG 4").AsString();
 
-                            secondSideComments = string.Empty;
+                                secondSideComments = string.Empty;
 
-                            if (!string.IsNullOrEmpty(tag1)) secondSideComments += tag1;
-                            if (!string.IsNullOrEmpty(tag2)) secondSideComments += "_" + tag2;
-                            if (!string.IsNullOrEmpty(tag3)) secondSideComments += "_" + tag3;
-                            if (!string.IsNullOrEmpty(tag4)) secondSideComments += "_" + tag4;
+                                if (!string.IsNullOrEmpty(tag1)) secondSideComments += tag1;
+                                if (!string.IsNullOrEmpty(tag2)) secondSideComments += "_" + tag2;
+                                if (!string.IsNullOrEmpty(tag3)) secondSideComments += "_" + tag3;
+                                if (!string.IsNullOrEmpty(tag4)) secondSideComments += "_" + tag4;
+                            }
                         }
 
                         if (string.IsNullOrEmpty(firstSideComments) && string.IsNullOrEmpty(secondSideComments))
