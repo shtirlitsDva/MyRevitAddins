43dda216685b6996fc6aeee6176b592c6c8ffed2
Michail Golubjev <xopkop@gmail.com>
2020-10-22 18:17:05 +0200

    Removed hent data again.

--- MyRevitAddins/HentRestData/Form1.Designer.cs
+++ MyRevitAddins/HentRestData/Form1.Designer.cs
@@ -37,9 +37,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(22, 22);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 58);
+            this.button1.Size = new System.Drawing.Size(249, 107);
             this.button1.TabIndex = 0;
             this.button1.Text = "Where to save";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,48 +48,54 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(154, 12);
+            this.button2.Location = new System.Drawing.Point(282, 22);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(136, 58);
+            this.button2.Size = new System.Drawing.Size(249, 107);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(296, 12);
+            this.button3.Location = new System.Drawing.Point(543, 22);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(136, 58);
+            this.button3.Size = new System.Drawing.Size(249, 107);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(438, 12);
+            this.button4.Location = new System.Drawing.Point(803, 22);
+            this.button4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(136, 58);
+            this.button4.Size = new System.Drawing.Size(249, 107);
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 154);
+            this.textBox1.Location = new System.Drawing.Point(22, 284);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(776, 20);
+            this.textBox1.Size = new System.Drawing.Size(1419, 29);
             this.textBox1.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1467, 831);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
--- MyRevitAddins/HentRestData/Form1.cs
+++ MyRevitAddins/HentRestData/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IdentityModel.Tokens;
+using System.ServiceModel.Security;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,6 +11,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using mySettings = HentRestData.Properties.Settings;
+using OfficeDevPnP.Core.IdentityModel.WSTrustBindings;
+using Microsoft.IdentityModel.SecurityTokenService;
+using System.ServiceModel;
 
 namespace HentRestData
 {
@@ -16,6 +21,7 @@ namespace HentRestData
     {
         public string pathToSave;
         public string fullPathAndName;
+        private string Token;
         public Form1()
         {
             InitializeComponent();
@@ -43,5 +49,97 @@ namespace HentRestData
         {
             mySettings.Default.Save();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Get SAML2 token
+            GenericXmlSecurityToken saml2Token;
+            string ADFS_URL = "https://fs.datafordeler.dk/adfs/services/trust/13/usernamemixed";
+            string USER_NAME = "dfdprod01.sys\\MichailGolubje0Q1P7D";
+            string PASSWORD = "mgo123IT";
+
+            using (var factory =
+                new WSTrustChannelFactory(
+                    new UserNameWSTrustBinding(SecurityMode.TransportWithMessageCredential),
+                    new EndpointAddress(ADFS_URL)))
+
+            {
+                factory.TrustVersion = TrustVersion.WSTrust13;
+
+                factory.Credentials.UserName.UserName = USER_NAME;
+                factory.Credentials.UserName.Password = PASSWORD;
+
+                RequestSecurityToken rst = new RequestSecurityToken
+                {
+                    RequestType = RequestTypes.Issue,
+                    AppliesTo = new EndpointReference(REALM),
+                    KeyType = KeyTypes.Bearer,
+                    TokenType = "urn:oasis:names:tc:SAML:2.0:assertion",
+                };
+
+                IWSTrustChannelContract channel = factory.CreateChannel();
+                saml2Token = channel.Issue(rst) as GenericXmlSecurityToken;
+            }
+
+        //Convert token
+        string saml2TokenBase64 =
+            Convert.ToBase64String(
+            System.Text.Encoding.UTF8.GetBytes(saml2Token.TokenXml.OuterXml.ToCharArray()));
+
+
     }
+
+    //Convert token
+    string saml2TokenBase64 =
+        Convert.ToBase64String(
+        System.Text.Encoding.UTF8.GetBytes(saml2Token.TokenXml.OuterXml.ToCharArray()));
+}
+
+    //string ADFS_URL = "https://fs.datafordeler.dk/adfs/services/trust/13/usernamemixed";
+    //string REALM = "urn:dk:kmd:dd:value:sp:switchboard:knownuser";
+    //string USER_NAME = "dfdprod01.sys\\MichailGolubje0Q1P7D"
+    //string PASSWORD = "yyy";
+    //Uri BASE_ADDRESS = new Uri("https://services.datafordeler.dk");
+    //string REQUEST_URI
+    //= "/GeoDK/topo_skaermkort/1/wms/?service=WMS&version=1.3.0&LAYERS=dtk_skaermkort&FORMAT=IMAGE/PNG&REQUEST=GetMap&STYLES=&CRS=EPSG:25832&WIDTH=800&HEIGHT=545&BBOX=196364,5952066,923636,6447934&hest100=10";
+
+
+    ////Get SAML2 token
+    //GenericXmlSecurityToken saml2Token;
+
+    //using (var factory = new WSTrustChannelFactory(
+    //new UserNameWSTrustBinding(SecurityMode.TransportWithMessageCredential),
+    //new EndpointAddress(ADFS_URL))
+
+    //{
+    //    factory.TrustVersion = TrustVersion.WSTrust13;
+
+    //    factory.Credentials.UserName.UserName = USER_NAME;
+    //    factory.Credentials.UserName.Password = PASSWORD;
+
+    //    RequestSecurityToken rst = new RequestSecurityToken
+    //    {
+    //        RequestType = RequestTypes.Issue,
+    //        AppliesTo = new EndpointReference(REALM),
+    //        KeyType = KeyTypes.Bearer,
+    //        TokenType = "urn:oasis:names:tc:SAML:2.0:assertion",
+    //    };
+
+    //    IWSTrustChannelContract channel = factory.CreateChannel();
+    //    saml2Token = channel.Issue(rst) as GenericXmlSecurityToken;
+    //}
+
+    ////Convert token
+    //string saml2TokenBase64 =
+    //    Convert.ToBase64String(
+    //    System.Text.Encoding.UTF8.GetBytes(saml2Token.TokenXml.OuterXml.ToCharArray()));
+
+    ////request a resource with the token
+    //HttpClient client = new HttpClient();
+    //client.DefaultRequestHeaders.Authorization
+    //    = new AuthenticationHeaderValue("Bearer", saml2TokenBase64);
+    //client.BaseAddress = BASE_ADDRESS;
+    //HttpResponseMessage httpResponseMessage = await client.GetAsync(REQUEST_URI);
+
+}
 }
