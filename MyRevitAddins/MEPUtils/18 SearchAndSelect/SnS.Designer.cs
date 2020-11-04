7c948fde65ea4e04efef20a92a04ef845b5a680d
Michail Golubjev <xopkop@gmail.com>
2020-11-04 21:42:51 +0100

    SNS: start

---
+++ MyRevitAddins/MEPUtils/18 SearchAndSelect/SnS.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MEPUtils._18_SearchAndSelect
+{
+    partial class SnS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Select = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.tabControl1.SuspendLayout();
+            this.Select.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Select);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(2196, 1419);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Select
+            // 
+            this.Select.BackColor = System.Drawing.Color.DarkGray;
+            this.Select.Controls.Add(this.checkedListBox1);
+            this.Select.Location = new System.Drawing.Point(4, 33);
+            this.Select.Name = "Select";
+            this.Select.Padding = new System.Windows.Forms.Padding(3);
+            this.Select.Size = new System.Drawing.Size(2188, 1382);
+            this.Select.TabIndex = 0;
+            this.Select.Text = "Select";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 33);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 63);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(268, 191);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(478, 472);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // SnS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2196, 1419);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "SnS";
+            this.Text = "SnS";
+            this.tabControl1.ResumeLayout(false);
+            this.Select.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Select;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
