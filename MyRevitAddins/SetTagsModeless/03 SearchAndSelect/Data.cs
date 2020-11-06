1653055a805700eee9fc1009335f3587f5a970f9
Michail Golubjev <xopkop@gmail.com>
2020-11-06 22:46:04 +0100

    SnS: cont 1

---
+++ MyRevitAddins/SetTagsModeless/03 SearchAndSelect/Data.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk;
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.UI.Selection;
+
+namespace MEPUtils.ModelessForms.SearchAndSelect
+{
+    public static class Data
+    {
+        public static Dictionary<string, BuiltInCategory> NamesOfCats = new Dictionary<string, BuiltInCategory>
+        {
+            
+        };
+    }
+}
