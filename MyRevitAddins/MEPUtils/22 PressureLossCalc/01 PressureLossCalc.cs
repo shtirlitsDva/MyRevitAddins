6a214cd7312653c6466d782c540f3242cc695be6
Michail Golubjev <xopkop@gmail.com>
2022-11-16 23:44:52 +0100

    Maybeeeeh, baaaybeeeh

---
+++ MyRevitAddins/MEPUtils/22 PressureLossCalc/01 PressureLossCalc.cs
@@ -0,0 +1,58 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using MEPUtils.SharedStaging;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using MoreLinq;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Windows.Input;
+using dbg = Shared.Dbg;
+using fi = Shared.Filter;
+using lad = MEPUtils.CreateInstrumentation.ListsAndDicts;
+using mp = Shared.MepUtils;
+using tr = Shared.Transformation;
+
+namespace MEPUtils.PressureLossCalc
+{
+    public class PressureLossCalc
+    {
+        public Result PressureLossCalcMethod(ExternalCommandData commandData)
+        {
+            UIApplication uiApp = commandData.Application;
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            UIDocument uidoc = uiApp.ActiveUIDocument;
+
+            try
+            {
+                using (Transaction tx = new Transaction(doc))
+                {
+                    var allFittings = fi.GetElements<Element, BuiltInCategory>(doc, BuiltInCategory.OST_PipeFitting);
+
+                    tx.Start("");
+                    
+
+                    tx.Commit();
+                    return Result.Succeeded; 
+                }
+                
+            }
+
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException) { return Result.Cancelled; }
+
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
